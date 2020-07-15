8fffce59daec2a8c7a9cad5fc563ffd9bc22ffc1
shawon <shawon5ice@gmail.com>
2020-07-15 20:08:52 +0600

    added custom user model resolved try 1

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,\
+                                            PermissionsMixin
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -21,9 +23,10 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email insted of username"""
-    email = models.EmailField(max_length=255,unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default = True)
     is_stuff = models.BooleanField(default=False)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,17 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
         email = 'test@gmail.com'
         password = 'Test123'
-
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email= email,
+            password= password
         )
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
@@ -19,7 +18,6 @@ class ModelTests(TestCase):
         """Test the email for a new user is normalized"""
         email = 'test@LONDONAPPDEV.com'
         user = get_user_model().objects.create_user(email, 'test123')
-
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
@@ -34,5 +32,5 @@ class ModelTests(TestCase):
             'test123'
         )
 
-        self.assertTrue(user.is_superuser);
+        self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_stuff)
